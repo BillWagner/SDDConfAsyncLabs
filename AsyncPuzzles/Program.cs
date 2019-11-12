8ab5336f85501da7a817c6d3dfdcfac5bd315e71
Bill Wagner <bill.w.wagner@outlook.com>
2019-11-12 11:05:51 +0000

    add puzzle starter

---
+++ AsyncPuzzles/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncPuzzles
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var msgTask = DoRemoteWork("a new customer");
+            Console.WriteLine("Doing remote work");
+            var msg = await msgTask;
+            Console.WriteLine(msg);
+
+        }
+
+        private static Task<string> DoRemoteWork(string customer)
+        {
+            using (var service = new RemoteService())
+            {
+                return service.GenerateMessageAsync(customer);
+            }
+        }
+    }
+}
---
+++ AsyncPuzzles/RemoteService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncPuzzles
+{
+    public class RemoteService : IDisposable
+    {
+        private bool disposed;
+
+        public virtual void Dispose()
+        {
+            disposed = true;
+        }
+
+        public async Task<string> GenerateMessageAsync(string customer)
+        {
+            await Task.Yield();
+            if (disposed)
+            {
+                throw new ObjectDisposedException("server", "The remote server has been disposed");
+            }
+            return $"Remote work done asynchronously for {customer}";
+        }
+    }
+}
